290fa775bb844b3b2f0f20f0c18ad418e0a7c69a
Andres Mosquera Alvarado <mosquera.andres@correounivalle.edu.co>
2026-06-05 17:31:12 -0500

    Adding @ServiceConnection instead of DynamicPropertySouce to get the datasource properties

--- src/test/java/com/dev/mosquera/usermanager/repository/UserRepositoryContainerTest.java
+++ src/test/java/com/dev/mosquera/usermanager/repository/UserRepositoryContainerTest.java
@@ -26,17 +26,11 @@ public class UserRepositoryContainerTest {
     private UserRepository repository;
 
     @Container
+    @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
             .withDatabaseName("testdb")
             .withUsername("test")
             .withPassword("test");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     @Test
     void shouldSaveAndFindUserById() {
