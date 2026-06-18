ee9268d70cd16636694c4874b0e4f563ad2a8aa7
Andres Mosquera Alvarado <mosquera.andres@correounivalle.edu.co>
2026-06-18 17:57:27 -0500

    Adding some change to the custom user details

--- src/main/java/com/dev/mosquera/usermanager/service/implementation/CustomUserDetailsService.java
+++ src/main/java/com/dev/mosquera/usermanager/service/implementation/CustomUserDetailsService.java
@@ -1,12 +1,24 @@
 package com.dev.mosquera.usermanager.service.implementation;
 
+import com.dev.mosquera.usermanager.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = repository.findUserByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + username));
+        return ;
     }
 }
